e3a6cf270d29234068c895c96b8eb39d6be77dac
Kuda <zaulbushta@gmail.com>
2022-10-22 12:20:39 -0500

    Commit inicial

---
+++ suma.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+/*
+ * Autor: Saúl Bustamante Bernabe
+ * Descripción: Suma
+ * Carrera: Ingenieria de Software
+ * Grupo: 1D
+ * */
+
+void main(){
+    int a = 23;
+    int b = 75;
+    int r = a + b;
+    printf("%d + %d = %d\n", a, b, r);
+}
+
