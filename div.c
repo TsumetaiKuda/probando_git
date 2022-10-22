beac8597d1487ca0cd034ddd594c449cbe20f4d8
Kuda <zaulbushta@gmail.com>
2022-10-22 15:58:12 -0500

    Agregando division desde rama-c

---
+++ div.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+/*
+ * Autor: Saúl Bustamante Bernabe
+ * Descripción: División
+ * Carrera: Ingenieria de Software
+ * Grupo: 1D
+ * */
+
+void main(){
+    float a = 12.4;
+    float b = 947;
+    float r = a / b;
+    printf("%f / %f = %f", a, b, r);
+}
+
