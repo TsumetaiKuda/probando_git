9b3527560d46b9b678e29da501b428a593f916d1
Kuda <zaulbushta@gmail.com>
2022-10-22 13:23:27 -0500

    Agregando archivo de mult.c desde otra la rama-b

---
+++ mult.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+/*
+ * Autor: Saúl Bustamante Bernabe
+ * Descripción: Multiplicación
+ * Carrera: Ingenieria de Software
+ * Grupo: 1D
+ * */
+
+void main(){
+    int a = 123;
+    int b = 93;
+    int r = a * b;
+    printf("%d * %d = %d\n", a, b, r);
+}
+
