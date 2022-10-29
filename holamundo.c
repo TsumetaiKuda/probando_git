ea17f659d0bd306628bbac4040f07282cc3cfae7
Kuda <zaulbushta@gmail.com>
2022-10-29 13:14:35 -0500

    Solo un holamundo

---
+++ holamundo.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+/*
+ * Autor: Saúl Bustamante Bernabe
+ * Descripción: 
+ * Carrera: Ingenieria de Software
+ * Grupo: 1D
+ * */
+
+void main(){
+    printf("Hola mundo\n|");
+}
+
